848c79983eda5d66bb373a551cb75c6e3920ac89
julio <julio458.h@gmail.com>
2021-11-03 15:51:54 -0600

    v1.1

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+#Archivo prueba.sql
+script necesario para la creacion de la base de datos, tablas y regristro que se 
+consumen en la aplicacion.
---
+++ pruebadb.sql
@@ -0,0 +1,46 @@
+CREATE SCHEMA `pruebadb` ;
+
+USE pruebadb;
+
+CREATE TABLE `pruebadb`.`persona` (
+  `id_persona` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `saldo` double DEFAULT NULL,
+  PRIMARY KEY (`id_persona`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+);
+
+
+CREATE TABLE `pruebadb`.`usuario` (
+  `id_usuario` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `password` VARCHAR(128) NULL,
+  PRIMARY KEY (`id_usuario`));
+  
+  
+  CREATE TABLE `pruebadb`.`rol` (
+  `id_rol` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `id_usuario` INT NULL,
+  PRIMARY KEY (`id_rol`),
+  INDEX `fk_rol_usuario_idx` (`id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_rol_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `pruebadb`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+    INSERT INTO `pruebadb`.`usuario` (`username`, `password`) VALUES ('admin', '$2a$10$1kPqcX/kz30LfN1GuCmZlOOzd7Lm8H4XW/WD05w39ssJ0Rq5OcAA6');
+    INSERT INTO `pruebadb`.`usuario` (`username`, `password`) VALUES ('user1', '$2a$10$lSvpEKeaReHQvkkw3TfP..DJIcwi.YxJLp30106ge0aBnVgMJrHry');
+    
+    INSERT INTO `pruebadb`.`rol` (`nombre`, `id_usuario`) VALUES ('ROLE_ADMIN', '1');
+    INSERT INTO `pruebadb`.`rol` (`nombre`, `id_usuario`) VALUES ('ROLE_USER', '1');
+    INSERT INTO `pruebadb`.`rol` (`nombre`, `id_usuario`) VALUES ('ROLE_USER', '2');
+
+
+    select * from pruebadb.persona;
+  
+  
